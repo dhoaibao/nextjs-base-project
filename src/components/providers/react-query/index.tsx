c931553d3f51d4fef01ccf8c7527e51989907f8f
Duong Hoai Bao <bao.duong@hdwebsoft.dev>
2025-06-19 16:24:45 +0700

    feat: integrate React Query

---
+++ src/components/providers/react-query/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000, // 60 seconds
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | null = null;
+
+function getQueryClient() {
+  if (typeof window === 'undefined' || isServer) {
+    return makeQueryClient();
+  }
+
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient();
+  }
+
+  return browserQueryClient;
+}
+
+const ReactQueryProvider = ({ children }: Props) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
